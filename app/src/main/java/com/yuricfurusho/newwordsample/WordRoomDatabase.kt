779e2081cdde015ee1df50ac38f312dff03314b5
Yuri Furusho <yuricfurusho@gmail.com>
2018-09-25 23:47:48 -0300

    7. Add a Room database

---
+++ app/src/main/java/com/yuricfurusho/newwordsample/WordRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.yuricfurusho.newwordsample
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+
+/**
+ * Created by Yuri Furusho on 25/09/18.
+ */
+@Database(entities = arrayOf(Word::class), version = 1)
+public abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                // Create database here
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        WordRoomDatabase::class.java,
+                        "Word_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
