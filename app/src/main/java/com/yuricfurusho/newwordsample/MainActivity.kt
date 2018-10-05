146226ef1cfa6921b1488094e3fda61ddb2cb191
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-10-05 12:42:25 -0300

    11. Add a RecyclerView

--- app/src/main/java/com/yuricfurusho/newwordsample/MainActivity.kt
+++ app/src/main/java/com/yuricfurusho/newwordsample/MainActivity.kt
@@ -3,6 +3,8 @@ package com.yuricfurusho.newwordsample
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 
@@ -19,6 +21,11 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
