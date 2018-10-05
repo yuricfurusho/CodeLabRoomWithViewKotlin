1bf4148d1c62f9fe6b3c74d23ff5550eebe8e1a8
Yuri Furusho <yuricfurusho@gmail.com>
2018-10-05 08:05:37 -0300

    9. Create the ViewModel

--- app/src/main/java/com/yuricfurusho/newwordsample/WordRepository.kt
+++ app/src/main/java/com/yuricfurusho/newwordsample/WordRepository.kt
@@ -9,6 +9,7 @@ import android.support.annotation.WorkerThread
 class WordRepository(private val wordDao: WordDao) {
 
     val allWords: LiveData<List<Word>> = wordDao.getAllWords()
+
     @WorkerThread
     suspend fun insert(word: Word) {
         wordDao.insert(word)
---
+++ app/src/main/java/com/yuricfurusho/newwordsample/WordViewModel.kt
@@ -0,0 +1,37 @@
+package com.yuricfurusho.newwordsample
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.Main
+import kotlin.coroutines.experimental.CoroutineContext
+
+/**
+ * Created by Yuri Furusho on 04/10/18.
+ */
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = scope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
